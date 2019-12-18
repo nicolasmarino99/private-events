8d60860f5bd678560324c50cf01c6c0249fb4e05
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2019-12-18 15:08:30 -0500

    add athentication methods; create session cookies; create log_in state with session

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'static_pages/organize'
   get 'static_pages/help'
   get 'static_pages/create_event'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
