c44ad03d3defb000ac1ca65e0d19f8e3ae9f0158
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2019-12-18 12:27:37 -0500

    create user validations; create user error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,22 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(users_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  private
+
+    def users_params
+      params.require(:user).permit(:name, :email, :password,:password_confirmation)
+    end
+
 end
