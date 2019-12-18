6199a16a09f205dfadb6817fad5fd78a084843c3
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2019-12-18 14:18:37 -0500

    Use SSL and the Puma webserver in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,6 +101,8 @@ Rails.application.configure do
   # timestamps for the last write to the primary. The resolver uses the context
   # class timestamps to determine how long to wait before reading from the
   # replica.
+  config.force_ssl = true
+
   #
   # By default Rails will store a last write timestamp in the session. The
   # DatabaseSelector middleware is designed as such you can define your own
