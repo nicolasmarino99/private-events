d45f412eb893f4e8331af8d89f0fb2e89c5fd57d
Bruna <brunagenz92@gmail.com>
2019-12-20 16:57:02 -0400

    define before filter to restric new events to logged in users; change links to create event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
+  before_action :logged_in_user, only: [:new, :create]
+  
   def show
     @event = Event.find(params[:id])
   end
@@ -8,8 +10,9 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = Event.new(events_params)
-    if @event.save
+    @event = current_user.events.build(events_params)
+    if @event.valid?
+      @event.save
       flash[:sucess] = "Succesfull event creation"
       redirect_to root_path
     else
@@ -26,6 +29,11 @@ class EventsController < ApplicationController
 
       def events_params
         params.require(:event).permit(:name, :date, :location,:description, :tag)
+      end
+
+      def logged_in_user
+        flash[:danger] = "You must sign in first"
+        redirect_to login_path
       end
 
 
