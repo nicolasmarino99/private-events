6df3d3ad03a10e616fc8af69597ef1ae31c71da4
Bruna <brunagenz92@gmail.com>
2019-12-20 14:20:57 -0400

    fix association between event and creator; display users' created events on user show view

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,5 @@ class User < ApplicationRecord
     # Associations
     
     has_one_attached :profile_picture
-    has_many :created_events, foreing_key: "creator_id", class_name: "Event"
+    has_many :events, foreign_key: "creator_id"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_192615) do
+ActiveRecord::Schema.define(version: 2019_12_20_175628) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2019_12_19_192615) do
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
